0ff82311aad52d92c3c27ed275eda44b083b52f7
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-07-24 20:19:51 +0200

    Switched to english

--- src/main/java/eu/baumistlustig/gamemode/commands/Start.java
+++ src/main/java/eu/baumistlustig/gamemode/commands/Start.java
@@ -1,12 +1,14 @@
 package eu.baumistlustig.gamemode.commands;
 
 import eu.baumistlustig.gamemode.Gamemode;
+import eu.baumistlustig.gamemode.utils.Timer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,12 +20,15 @@ public class Start implements CommandExecutor {
         time = 15;
 
         BukkitRunnable runnable = new BukkitRunnable() {
+
+            final Timer timer = Gamemode.getPlugin().getTimer();
+
             @Override
             public void run() {
 
                 switch (time) {
                     case 15:
-                        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "GAME" + ChatColor.GRAY + "]" + " Das Spiel startet in " + ChatColor.GREEN + time + ChatColor.GRAY + " Sekunden!");
+                        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "GAME" + ChatColor.GRAY + "]" + " Game starts in " + ChatColor.GREEN + time + ChatColor.GRAY + " seconds!");
                         Bukkit.getOnlinePlayers().forEach(player -> {
                             player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BIT, 2,1);
                         });
@@ -32,23 +37,31 @@ public class Start implements CommandExecutor {
                     case 3:
                     case 4:
                     case 2: {
-                        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "GAME" + ChatColor.GRAY + "]" + " Noch " + ChatColor.GREEN + time + ChatColor.GRAY + " Sekunden übrig!");
+                        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "GAME" + ChatColor.GRAY + "]" + " Only " + ChatColor.GREEN + time + ChatColor.GRAY + " second left!");
                         Bukkit.getOnlinePlayers().forEach(player -> {
                             player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BIT, 2,1);
                         });
                         break;
                     }
                     case 1: {
-                        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "GAME" + ChatColor.GRAY + "]" + " Noch " + ChatColor.GREEN + time + ChatColor.GRAY + " Sekunde übrig!");
+                        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "GAME" + ChatColor.GRAY + "]" + " Only " + ChatColor.GREEN + time + ChatColor.GRAY + " seconds left!");
                         Bukkit.getOnlinePlayers().forEach(player -> {
                             player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BIT, 2,1);
                         });
                         break;
                     }
                     case 0:
-                        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "GAME" + ChatColor.GRAY + "]" + ChatColor.GREEN + " Das Spiel startet" + ChatColor.BOLD +  " jetzt" + ChatColor.RESET + "" + ChatColor.GRAY + "!");
+                        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "GAME" + ChatColor.GRAY + "]" + ChatColor.GREEN + " Game starts" + ChatColor.BOLD +  " now" + ChatColor.RESET + "" + ChatColor.GRAY + "!");
                         Bukkit.getOnlinePlayers().forEach(player -> {
                             player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BIT, 3,2);
+                        });
+                        for (Player p : Bukkit.getOnlinePlayers()) {
+                            p.sendTitle(ChatColor.GREEN.toString() + ChatColor.BOLD + "Let's Go!", ChatColor.GRAY + "Game starts now");
+                        }
+                        timer.setRunning(true);
+                        Bukkit.getOnlinePlayers().forEach(player -> {
+                            player.setExp(0);
+                            player.setLevel(0);
                         });
                         cancel();
                         return;
